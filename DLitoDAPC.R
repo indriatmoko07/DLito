a3879d1aa6d12f329cd3b7c54085035a6b64bc29
Indriatmoko <88871879+indriatmoko07@users.noreply.github.com>
2024-02-12 07:53:49 +0700

    Update DLitoDAPC.R

--- DLitoDAPC.R
+++ DLitoDAPC.R
@@ -11,4 +11,19 @@ DLitoDAPC<- function(x){
   scatter(dapc_result, posi.da="topright", bg="white",
           pch=numPCs, cstar=1, col=myCol, scree.pca=TRUE,
           posi.pca="topleft")
+  dapc.results <- as.data.frame(dapc_result$posterior)
+  dapc.results$pop <- pop(glSNP)
+  dapc.results$indNames <- rownames(dapc.results)
+  library(tidyr)
+  dapc.results <- pivot_longer(dapc.results, -c(pop, indNames))
+  head(dapc.results, n = 6)
+  colnames(dapc.results) <- c("Original_Pop","Sample","Assigned_Pop","Posterior_membership_probability")
+  
+  p <- ggplot(dapc.results, aes(x=Sample, y=Posterior_membership_probability, fill=Assigned_Pop))
+  p <- p + geom_bar(stat='identity')
+  p <- p + scale_fill_manual(values = cols)
+  p <- p + facet_grid(~Original_Pop, scales = "free")
+  p <- p + theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 8))
+  p
 }
+
